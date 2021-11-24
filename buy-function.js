4bb6e9094a5fa6ab88ad7518f6b7c4c58b4886cc
Rodrigo Costa <rod@nesasolutions.com>
2021-11-24 15:25:47 -0300

    Improvements to and asking parameters

--- buy-function.js
+++ buy-function.js
@@ -1,39 +1,46 @@
-let buyed = false;
-function buy() {
+let buyed;
+async function buy() {
+  buyed = false;
+  const maxPrice = Number.parseInt(prompt('Preço maximo a pagar: ')) || 0;
+  const minQty = Number.parseInt(prompt('Quantidade de itens minima: ')) || 1;
+  const sleep = interval => new Promise(res => setTimeout(res, interval));
   var btnSearch = document.getElementById('searchmarket');
+  console.warn('Parameters', 'maxPrice: ', maxPrice, 'minQty', minQty)
 
-  const interval = setInterval(() => {
-    if (!buyed) {
-      btnSearch.click();
-  
-      setTimeout(() => {
-        let onSale = Array.from(document.getElementsByClassName('pricefish'));
-  
-        console.warn('ELEMENTS ON SALE: ', onSale, onSale.length);
+  while(!buyed) {
+    btnSearch.click();
     
-        let buyThis = 
-          onSale.find(element => {
-            const isSold = element.classList.contains('sold');
-            const isOnPriceDesidered = Array.from(element.getElementsByTagName('font')).some(font => Number.parseInt(font.innerText <= 2));
-            console.warn('checking: ', element, 'isSold?', isSold, 'isOnPriceDesidered', isOnPriceDesidered, Array.from(element.getElementsByTagName('font')).map(f => f.innerText));
-            return !isSold && isOnPriceDesidered;
-          });
-        
-        if (buyThis) {
-          console.warn('FOUND AN ITEM: ', buyThis);
-          buyThis.click();
+    await sleep(500);
+    let cardsOnSale = Array.from(document.getElementsByClassName('cardfish'));
+
+    console.warn('ELEMENTS ON SALE: ', cardsOnSale.length);
+
+    let title = '';
+    let price = 0;
+    let buyThis = cardsOnSale.find(element => {
+      var elementPrice = element.querySelector('.pricefish');
+      var isSold = elementPrice.classList.contains('sold');
+      price = Number.parseInt(elementPrice.innerText);
+      var isOnPriceDesidered = (price <= maxPrice);
+
+      title = element.querySelector('span[id="idenfity"]').innerText;
+      var elementQty = Number.parseInt(title.match(/\[x(\d*)\]/)[1]);
+      var hasQtyDesired = (elementQty >= minQty);
+      var matchParams = !isSold && isOnPriceDesidered && hasQtyDesired;
+      console.warn('Title', title, 'Price', price, 'Qty', elementQty, 'isSold', isSold, 'Match params: ', matchParams);
+      return matchParams;
+    });
+    
+    if (buyThis) {
+      console.warn('Buying...');
+      buyThis.querySelector('.pricefish').click();
+      await sleep(500);
+        const btnBuy = document.querySelector('.buyitemshopm');
+        if (btnBuy) {
+          btnBuy.click();
           buyed = true;
-          setTimeout(() => {
-            const btnBuy = document.querySelector('.buyitemshopm');
-            if (btnBuy) {
-              btnBuy.click();
-              console.warn('BUYED SUCCESFULLY!');
-            }
-          }, 500);
+          alert(`Parabéns, comprado com sucesso. '${title}' por ${price} alga(s)`);
         }
-      }, 500);
-    } else {
-      clearInterval(interval);
     }
-  }, 2000);
+  }
 }
